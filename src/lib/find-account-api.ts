1fd1602e9217b411a76eb848ebd6849d41be6186
PARK GEONDO <116621655+GD000908@users.noreply.github.com>
2025-07-28 13:04:13 +0900

    비밀번호 찾기 api url 하드코딩으로 수정

--- src/lib/find-account-api.ts
+++ src/lib/find-account-api.ts
@@ -1,6 +1,6 @@
 // lib/find-account-api.ts
 //const API_BASE_URL = 'http://localhost:8080/api';
-const API_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const API_BASE_URL = `https://initback-production-67bf.up.railway.app/api`;
 
 
 export const findAccountApi = {
